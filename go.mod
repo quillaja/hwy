0756de8b237b073f60fed5a1fd6e3f741d7f2005
Ben <quillaja@hotmail.com>
2019-09-12 03:49:33 -0700

    viewer has interactivity

--- go.mod
+++ go.mod
@@ -9,12 +9,13 @@ require (
 	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
 	github.com/go-gl/glfw v0.0.0-20190409004039-e6da0acd62b1 // indirect
 	github.com/go-gl/mathgl v0.0.0-20190713194549-592312d8590a // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/quillaja/goutil v0.0.0-20180416090711-838533576f30
 	github.com/sergi/go-diff v1.0.0 // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/image v0.0.0-20190321063152-3fc05d484e9f
+	golang.org/x/image v0.0.0-20190910094157-69e4b8554b2a
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
 	googlemaps.github.io/maps v0.0.0-20190906051648-24f4c8471353
 )
--- viewer/main.go
+++ viewer/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"os"
+
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
+
+	"github.com/faiface/pixel/text"
 
 	"github.com/quillaja/hwy"
 
@@ -22,7 +29,7 @@ func kill(err error) {
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "hwy viewer",
-		Bounds: pixel.R(0, 0, 1200, 800),
+		Bounds: pixel.R(0, 0, 1000, 1000),
 		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
@@ -30,22 +37,34 @@ func run() {
 		panic(err)
 	}
 
+	// display parameters
+	const (
+		pointSearchDist = 10e3 // 10km radius
+		mapThickness    = 0.1
+		edgeThickness   = 0.05
+		gridThickness   = 0.1
+
+		mapscale   = 10.0
+		labelscale = 0.1
+	)
+	mmatrix := pixel.IM.Scaled(pixel.ZV, mapscale)
+
 	// get point data
 	file, err := os.Open("maps/countries/json/united_states.json")
 	kill(err)
 	usa := maps.StateFromJSON(file)
 	file.Close()
 
-	const mapscale = 20
 	// draw points into pixel object
 	shape := imdraw.New(nil)
 	shape.Color = colornames.Black
+	shape.SetMatrix(mmatrix)
 	for i := range usa.Polygons {
-		for j := range usa.Polygons[i] {
+		for j := 0; j < len(usa.Polygons[i]); j += 10 { // draw every Nth point
 			// x = longitude, y = latitude (reversed from "normal")
-			shape.Push(pixel.Vec{usa.Polygons[i][j][1], usa.Polygons[i][j][0]}.Scaled(mapscale))
+			shape.Push(pixel.Vec{usa.Polygons[i][j][1], usa.Polygons[i][j][0]}) //.Scaled(mapscale))
 		}
-		shape.Polygon(0.2)
+		shape.Polygon(mapThickness)
 	}
 
 	// get graph data
@@ -57,35 +76,49 @@ func run() {
 	// draw graph verticies
 	vertices := imdraw.New(nil)
 	vertices.Color = colornames.Blue
+	vertices.SetMatrix(mmatrix)
+
+	font, _ := truetype.Parse(goregular.TTF)
+	face := truetype.NewFace(font, &truetype.Options{Size: 32})
+	atlas := text.NewAtlas(face, text.ASCII)
+	labels := text.New(pixel.ZV, atlas)
+	labels.Color = colornames.Black
+
 	for _, place := range graph.Places() {
-		vertices.Push(pixel.Vec{place.Longitude, place.Latitude}.Scaled(mapscale))
-		vertices.Circle(0.8, 0)
+		p := pixel.Vec{place.Longitude, place.Latitude} //.Scaled(mapscale)
+		vertices.Push(p)
+		vertices.Circle(pointSearchDist*mTd(place.Latitude), 0)
+
+		labels.Dot = p.Scaled(mapscale / labelscale).Add(pixel.V(1, 1))
+		labels.WriteString(place.Name())
 	}
 	// draw graph edges
 	edges := imdraw.New(nil)
 	edges.Color = colornames.Red
+	edges.SetMatrix(mmatrix)
 	for orig, dests := range graph {
 		for dest := range dests {
-			edges.Push(pixel.Vec{orig.Longitude, orig.Latitude}.Scaled(mapscale))
-			edges.Push(pixel.Vec{dest.Longitude, dest.Latitude}.Scaled(mapscale))
-			edges.Line(0.2)
+			edges.Push(pixel.Vec{orig.Longitude, orig.Latitude}) //.Scaled(mapscale))
+			edges.Push(pixel.Vec{dest.Longitude, dest.Latitude}) //.Scaled(mapscale))
+			edges.Line(edgeThickness)
 		}
 	}
 
 	// make grid
 	grid := imdraw.New(nil)
 	grid.Color = colornames.Gray
-	const xmax = 200
+	grid.SetMatrix(mmatrix)
+	const xmax = 180
 	const ymax = 90
 	for x := -xmax; x <= xmax; x += 10 {
-		grid.Push(pixel.Vec{float64(x), ymax}.Scaled(mapscale))
-		grid.Push(pixel.Vec{float64(x), -ymax}.Scaled(mapscale))
-		grid.Line(1)
+		grid.Push(pixel.Vec{float64(x), ymax})  //.Scaled(mapscale))
+		grid.Push(pixel.Vec{float64(x), -ymax}) //.Scaled(mapscale))
+		grid.Line(gridThickness)
 	}
 	for y := -ymax; y <= ymax; y += 10 {
-		grid.Push(pixel.Vec{xmax, float64(y)}.Scaled(mapscale))
-		grid.Push(pixel.Vec{-xmax, float64(y)}.Scaled(mapscale))
-		grid.Line(0.2)
+		grid.Push(pixel.Vec{xmax, float64(y)})  //.Scaled(mapscale))
+		grid.Push(pixel.Vec{-xmax, float64(y)}) //.Scaled(mapscale))
+		grid.Line(gridThickness)
 	}
 
 	// make camera control
@@ -94,22 +127,79 @@ func run() {
 	cam.XExtents.Low = -180 * mapscale
 	cam.YExtents.High = 90 * mapscale
 	cam.YExtents.Low = -90 * mapscale
-	// cam.Position = pixel.V(-70*mapscale, 45*mapscale)
+	cam.ZExtents.High *= mapscale
+	cam.ZExtents.Low *= 1 / mapscale
+	// cam.Position = pixel.V(-90*mapscale, 38*mapscale)
 
-	for !win.Closed() {
+	overlay := imdraw.New(nil)
+	overlay.SetColorMask(colornames.Lime)
+	overlay.SetMatrix(mmatrix)
+	olP := func(p *hwy.Place) {
+		overlay.Push(pixel.Vec{p.Longitude, p.Latitude})
+		overlay.Circle(pointSearchDist*mTd(p.Latitude), 0)
+	}
+	olL := func(a, b *hwy.Place) {
+		overlay.Push(pixel.Vec{a.Longitude, a.Latitude})
+		overlay.Push(pixel.Vec{b.Longitude, b.Latitude})
+		overlay.Line(edgeThickness)
+	}
+	olClear := func() {
+		overlay.Clear()
+		overlay.Reset()
+	}
+
+	var selA, selB *hwy.Place
+
+	for !win.Closed() && !win.JustPressed(pixelgl.KeyEscape) {
 
 		if win.JustPressed(pixelgl.KeyHome) {
 			cam.Reset()
+		}
+		if win.JustPressed(pixelgl.MouseButtonLeft) {
+			p := cam.Unproject(win.MousePosition()).Scaled(1 / mapscale)
+			place, dist, found := graph.FindWithin(p.Y, p.X, pointSearchDist)
+
+			if found {
+				switch {
+				case selA == nil:
+					fmt.Println(place, dist) // print place within 10km of click
+					selA = &place
+					selB = nil
+					olP(selA)
+
+				case selA != nil:
+					if selB == nil || (place != *selB && place != *selA) {
+						data, ok := graph.Edge(*selA, place)
+						if ok {
+							selB = &place
+							fmt.Println("\t", place, data)
+							olClear()
+							olP(selA)
+							olP(selB)
+							olL(selA, selB)
+						}
+					}
+
+					olP(selA)
+				}
+			} else {
+				selA = nil
+				selB = nil
+				olClear()
+			}
 		}
 
 		cam.Update(win)
 		win.SetMatrix(cam.GetMatrix())
 
 		win.Clear(colornames.White)
+
 		grid.Draw(win)
 		shape.Draw(win)
 		edges.Draw(win)
 		vertices.Draw(win)
+		labels.Draw(win, pixel.IM.Scaled(labels.Orig, labelscale))
+		overlay.Draw(win)
 
 		win.Update()
 	}
@@ -117,4 +207,12 @@ func run() {
 
 func main() {
 	pixelgl.Run(run)
+}
+
+const degtorad = math.Pi / 180.0
+
+// Returns multiplier to convert Degrees to Meters at the given latitude.
+func mTd(latitude float64) (degreesPerMeter float64) {
+	const mPDegEquator = 111319.9
+	return 1 / (mPDegEquator * math.Cos(latitude*degtorad))
 }
